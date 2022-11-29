287f2b4247ba863f8c0f929630c0ccee0406005c
TRANHONGPHONG97 <phong12b5nd@gmail.com>
2022-11-29 11:36:24 +0700

    Update

--- src/main/java/com/example/demo/TinhTrungBinhCong.java
+++ src/main/java/com/example/demo/TinhTrungBinhCong.java
@@ -3,12 +3,12 @@ package com.example.demo;
 public class TinhTrungBinhCong {
 
     public static void main(String[] args) {
-        double[] arr = {19, 12.89, 16.5, 200, 13.7};
-        double total = 0;
+        int[] arr = {1,2,3,4,5,9};
+        int total = 0;
         for (int i = 0; i < arr.length; i++) {
             total = total + arr[i];
         }
-        double average = total / arr.length;
-        System.out.format("Kết quả là: %.3f", average);
+        int average = total / arr.length;
+        System.out.println(average);
     }
 }

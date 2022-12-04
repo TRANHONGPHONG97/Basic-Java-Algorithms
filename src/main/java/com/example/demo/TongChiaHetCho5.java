6a358ae297a4257d61bc9e6e294aad9314a5a670
TRANHONGPHONG97 <phong12b5nd@gmail.com>
2022-12-04 20:27:10 +0700

    Update

--- src/main/java/com/example/demo/TongChiaHetCho5.java
+++ src/main/java/com/example/demo/TongChiaHetCho5.java
@@ -5,19 +5,23 @@ import java.util.Scanner;
 public class TongChiaHetCho5 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Nhập n:");
+        System.out.println("Nhập n: ");
         int n = scanner.nextInt();
         int[] arr = new int[n];
         for (int i = 0; i < arr.length; i++) {
             System.out.println("Nhập phần tử thứ " + (i + 1) + " :");
             arr[i] = scanner.nextInt();
         }
+        System.out.println("Các số chia hết cho 5 là: ");
         int sum = 0;
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] % 5 == 0) {
-                sum += arr[i];
+        for (int j = 0; j < arr.length; j++) {
+            // nếu i không chia hết cho 5 thì quay lại vòng lặp for
+            if (arr[j] % 5 != 0) {
+                continue;
             }
+            sum += arr[j];
+            System.out.println(arr[j]);
         }
-        System.out.println("Tổng các phần tử chia hết cho 5 có trong mảng là: " + sum);
+        System.out.println("Tổng các số chia hết cho 5 là: " + sum);
     }
 }

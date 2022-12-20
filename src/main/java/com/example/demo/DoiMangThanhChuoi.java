d388de1b04ff0917084a3918ee7d96ad959f621c
TRANHONGPHONG97 <phong12b5nd@gmail.com>
2022-12-20 14:29:54 +0700

    Update

--- src/main/java/com/example/demo/DoiMangThanhChuoi.java
+++ src/main/java/com/example/demo/DoiMangThanhChuoi.java
@@ -3,12 +3,17 @@ package com.example.demo;
 public class DoiMangThanhChuoi {
 
     public static void main(String args[]) {
-        char[] ch = {'g', 'o', 'o', 'd', ' ', 'm', 'o', 'r', 'n', 'i', 'n', 'g'};
+//        char[] ch = {'g', 'o', 'o', 'd', ' ', 'm', 'o', 'r', 'n', 'i', 'n', 'g'};
+        char[] ch1 = {'0', '9', '7', '6', '9', '3', '4', '4', '6', '2'};
         // cách 1
-        String str = new String(ch);
-        System.out.println(str);
+//        String str = new String(ch);
+        String str1 = new String(ch1);
+//        System.out.println(str);
+        System.out.println(str1);
         // cách 2
-        String str2 = String.valueOf(ch);
-        System.out.println(str2);
+//        String str2 = String.valueOf(ch);
+        String str3 = String.valueOf(ch1);
+//        System.out.println(str2);
+        System.out.println(str3);
     }
 }
